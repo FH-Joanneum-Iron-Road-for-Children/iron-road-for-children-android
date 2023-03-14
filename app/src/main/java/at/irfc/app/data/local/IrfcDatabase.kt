029a273fed8f36ea69904b956ad07c4581b23302
Fabian Schedler <dev.fabian.schedler@gmail.com>
2023-03-14 21:03:05 +0100

    #14 add sample dao

--- app/src/main/java/at/irfc/app/data/local/IrfcDatabase.kt
+++ app/src/main/java/at/irfc/app/data/local/IrfcDatabase.kt
@@ -2,10 +2,15 @@ package at.irfc.app.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import at.irfc.app.data.local.dao.EventDao
 import at.irfc.app.data.local.entity.Event
 
 @Database(entities = [Event::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class IrfcDatabase : RoomDatabase() {
+
+    abstract fun eventDao(): EventDao
 
     companion object {
         const val DATABASE_NAME = "IrfcDB"
