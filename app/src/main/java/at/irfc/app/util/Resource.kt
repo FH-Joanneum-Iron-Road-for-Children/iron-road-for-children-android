acee3c69313e6b29c78c7dea10e5a616cfacc326
Fabian Schedler <dev.fabian.schedler@gmail.com>
2023-03-14 21:51:01 +0100

    #12 fix format

--- app/src/main/java/at/irfc/app/util/Resource.kt
+++ app/src/main/java/at/irfc/app/util/Resource.kt
@@ -6,8 +6,9 @@ sealed class Resource<T : Any> {
     abstract val data: T?
 
     class Success<T : Any>(override val data: T) : Resource<T>()
-    class Error<T : Any>(@StringRes val errorMessage: Int, override val data: T? = null) :
-        Resource<T>()
-
     class Loading<T : Any>(override val data: T? = null) : Resource<T>()
+    class Error<T : Any>(
+        @StringRes val errorMessage: Int,
+        override val data: T? = null
+    ) : Resource<T>()
 }
