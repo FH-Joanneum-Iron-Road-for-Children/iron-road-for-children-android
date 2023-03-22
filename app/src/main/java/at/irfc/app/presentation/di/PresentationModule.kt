c151e0d3096376759e04f1e54a2c31aea9844a46
metrysaa20 <antonius1999@yahoo.de>
2023-03-22 08:13:21 +0100

    #27 removed not existing class from PresentationModule

--- app/src/main/java/at/irfc/app/presentation/di/PresentationModule.kt
+++ app/src/main/java/at/irfc/app/presentation/di/PresentationModule.kt
@@ -1,11 +1,9 @@
 package at.irfc.app.presentation.di
 
-import at.irfc.app.presentation.map.MapViewModel
 import at.irfc.app.presentation.program.ProgramViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val presentationModule = module {
     viewModelOf(::ProgramViewModel)
-    viewModelOf(::MapViewModel)
 }
