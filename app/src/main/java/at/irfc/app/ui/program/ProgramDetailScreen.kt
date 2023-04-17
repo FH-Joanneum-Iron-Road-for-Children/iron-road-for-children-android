2ac6fa4630628b33a730fc85489430f4c8a9d06f
Tobias Kogler <tobias.kogler@edu.fh-joanneum.at>
2023-04-17 14:26:29 +0200

    #34 Program Detail UI Component

--- app/src/main/java/at/irfc/app/ui/program/ProgramDetailScreen.kt
+++ app/src/main/java/at/irfc/app/ui/program/ProgramDetailScreen.kt
@@ -1,32 +1,105 @@
 package at.irfc.app.ui.program
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import at.irfc.app.R
+import coil.compose.AsyncImage
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Composable
 @Destination
 fun ProgramDetailScreen(id: String) {
-    Box(
+    val paddingText = 16.dp
+    val padding = 8.dp
+    Card(
         modifier = Modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.errorContainer)
-            .padding(8.dp),
-        contentAlignment = Alignment.Center
+            .fillMaxSize()
+
     ) {
         Text(
             text = stringResource(R.string.header_programDetailScreen) + id,
-            color = MaterialTheme.colorScheme.error
+            modifier = Modifier.padding(paddingText)
         )
+
+        AsyncImage(
+            model = "https://picsum.photos/200",
+            contentDescription = "Some picture",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxWidth().height(150.dp)
+        )
+
+        Text(
+            text = "Uhrzeit",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(
+                start = paddingText,
+                end = paddingText,
+                top = 8.dp,
+                bottom = 0.dp
+            )
+        )
+
+        Text(
+            text = "Get time from backend",
+            modifier = Modifier.padding(
+                start = paddingText,
+                end = paddingText,
+                top = 0.dp,
+                bottom = 8.dp
+            )
+        )
+
+        Text(
+            text = "Get Stage from backend",
+            modifier = Modifier.padding(
+                start = paddingText,
+                end = paddingText,
+                top = 8.dp,
+                bottom = 0.dp
+            )
+        )
+
+        Text(
+            text = "Get Music Type from backend",
+            modifier = Modifier.padding(
+                start = paddingText,
+                end = paddingText,
+                top = 0.dp,
+                bottom = 8.dp
+            )
+
+        )
+
+        Text(
+            text = "Get description from backend",
+            modifier = Modifier.padding(paddingText)
+        )
+
+        Row() {
+            AsyncImage(
+                model = "https://picsum.photos/200",
+                contentDescription = "Some picture",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.padding(padding)
+            )
+
+            AsyncImage(
+                model = "https://picsum.photos/200",
+                contentDescription = "Some picture",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.padding(padding)
+            )
+        }
     }
 }
