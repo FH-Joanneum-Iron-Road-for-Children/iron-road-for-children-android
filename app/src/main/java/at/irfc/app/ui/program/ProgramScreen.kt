6fd925740edd2b27f930e8a12998d25cb12ee6d7
Adam Khalil <adam.khalil@edu.fh-joanneum.at>
2023-04-30 11:23:49 +0200

    changes applied minor fixes missing

--- app/src/main/java/at/irfc/app/ui/program/ProgramScreen.kt
+++ app/src/main/java/at/irfc/app/ui/program/ProgramScreen.kt
@@ -3,12 +3,17 @@ package at.irfc.app.ui.program
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import at.irfc.app.data.local.entity.EventCategory
@@ -16,11 +21,13 @@ import at.irfc.app.data.local.entity.EventWithDetails
 import at.irfc.app.generated.navigation.destinations.ProgramDetailScreenDestination
 import at.irfc.app.presentation.program.ProgramViewModel
 import at.irfc.app.util.Resource
+import coil.compose.AsyncImage
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.navigate
+import java.text.SimpleDateFormat
 import org.koin.androidx.compose.getViewModel
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -35,7 +42,7 @@ fun ProgramScreen(
     val selectedCategory = viewModel.selectedCategory.collectAsState().value
     val categories = viewModel.categoryList.collectAsState().value.filter { it != selectedCategory }
 
-    // Material 3 does not include a PullToRefresh right now // TODO replace when added
+// Material 3 does not include a PullToRefresh right now // TODO replace when added
     SwipeRefresh(
         modifier = Modifier.fillMaxSize(),
         state = rememberSwipeRefreshState(eventListResource is Resource.Loading),
@@ -55,14 +62,78 @@ fun ProgramScreen(
 
             eventListResource.data?.let { eventList ->
                 items(eventList, EventWithDetails::id) { event ->
-                    Text(
-                        text = event.title,
+                    Card(
+                        shape = RoundedCornerShape(20.dp),
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 10.dp
+                        ),
                         modifier = Modifier
-                            .padding(5.dp)
-                            .clickable {
-                                navController.navigate(ProgramDetailScreenDestination(event.id))
+                            .padding(bottom = 16.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .clickable {
+                                    navController.navigate(
+                                        ProgramDetailScreenDestination(event.id)
+                                    )
+                                }
+                                .fillMaxWidth()
+                        ) {
+// Box for the image
+                            Box(
+                                modifier = Modifier
+                                    .width(150.dp)
+                                    .aspectRatio(1f)
+                                    .clip(RoundedCornerShape(20.dp))
+                                    .background(Color.White)
+                            ) {
+                                AsyncImage(
+                                    model = event.image.path,
+                                    contentDescription = event.image.title,
+                                    contentScale = ContentScale.FillBounds,
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .clip(RoundedCornerShape(20.dp))
+
+                                )
                             }
-                    )
+
+// Text on the right side of the image
+                            Column(
+                                modifier = Modifier
+                                    .padding(
+                                        start = 16.dp,
+                                        top = 30.dp,
+                                        end = 30.dp,
+                                        bottom = 30.dp
+                                    )
+                                    .fillMaxWidth()
+                            ) {
+                                Text(
+                                    text = event.title,
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    maxLines = 2,
+                                    overflow = TextOverflow.Ellipsis
+                                )
+
+                                Spacer(modifier = Modifier.height(10.dp))
+
+                                Text(
+                                    text = "${SimpleDateFormat("HH:mm").format(event.startDate)}" +
+                                        " - " + SimpleDateFormat(
+                                            "HH:mm"
+                                        ).format(event.endDate),
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+
+                                Text(
+                                    text = "Stage: ${event.location.name}",
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
