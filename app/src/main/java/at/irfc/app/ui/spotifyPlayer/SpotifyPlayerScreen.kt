af8ad9bfe00c108124cdb7cc3d998764bc02843e
Rudi <rudolf.berger@edu.fh-joanneum.at>
2025-05-05 13:32:00 +0200

    add SpotifyPlayerScreen

---
+++ app/src/main/java/at/irfc/app/ui/spotifyPlayer/SpotifyPlayerScreen.kt
@@ -0,0 +1,49 @@
+package at.irfc.app.ui.spotifyPlayer
+
+import android.webkit.WebView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.NavController
+import com.ramcosta.composedestinations.annotation.Destination
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Destination
+fun SpotifyPlayerScreen(playlistId: String, navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Spotify Playlist") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Zurück")
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        AndroidView(
+            factory = { context ->
+                WebView(context).apply {
+                    settings.javaScriptEnabled = true
+                    settings.domStorageEnabled = true
+                    loadUrl("https://open.spotify.com/embed/playlist/$playlistId")
+                }
+            },
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        )
+    }
+}
