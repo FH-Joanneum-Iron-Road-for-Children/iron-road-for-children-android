27257afc8ac325aa94e67ea81e27dd45e276bb4f
Rudi <rudolf.berger@edu.fh-joanneum.at>
2025-05-08 19:25:57 +0200

    new SplashScreen for Android 12

---
+++ app/src/main/java/at/irfc/app/ui/splashScreen/FullSplashScreen.kt
@@ -0,0 +1,51 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import at.irfc.app.R
+
+@Composable
+fun FullSplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(R.drawable.splash_logo),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = painterResource(R.drawable.splash_fhj_logo),
+                contentDescription = null,
+                modifier = Modifier.height(40.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Image(
+                painter = painterResource(R.drawable.splash_logo_text),
+                contentDescription = null,
+                modifier = Modifier.height(20.dp)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Image(
+                painter = painterResource(R.drawable.splash_footer),
+                contentDescription = null,
+                modifier = Modifier.height(33.dp)
+            )
+        }
+    }
+}
