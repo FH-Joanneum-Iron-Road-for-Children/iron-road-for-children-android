7de6af8a2440d83cbb97ca0daf2d62a8f58b30c5
Susanne Jandl <susanne.jandl@edu.fh-joanneum.at>
2025-05-12 12:22:36 +0200

    Add buttons

--- app/src/main/java/at/irfc/app/ui/aboutUs/AboutUsScreen.kt
+++ app/src/main/java/at/irfc/app/ui/aboutUs/AboutUsScreen.kt
@@ -1,21 +1,28 @@
 package at.irfc.app.ui.aboutUs
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ChildCare
+import androidx.compose.material.icons.outlined.DirectionsCarFilled
 import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.QueueMusic
 import androidx.compose.material.icons.outlined.Shield
+import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -29,7 +36,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -56,7 +65,6 @@ fun AboutUsScreen(repository: PlaylistRepository = koinInject(), navController: 
             playlist = result.data
         }
     }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -66,17 +74,49 @@ fun AboutUsScreen(repository: PlaylistRepository = koinInject(), navController: 
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val uriHandler = LocalUriHandler.current
-
-        Spacer(modifier = Modifier.height(20.dp))
+        // Spacer(modifier = Modifier.height(20.dp))
         ExpandableCard(
-            modifier = Modifier.padding(bottom = 15.dp),
-            unexpandedLines = 5,
+            unexpandedLines = 3,
             text = stringResource(R.string.aboutUs_text)
         )
-
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(3f)
+                    .aspectRatio(3f / 2f),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.shs),
+                    contentDescription = stringResource(R.string.shs),
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Fit
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Box(
+                modifier = Modifier
+                    .weight(2f)
+                    .aspectRatio(3f / 2f),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.lid),
+                    contentDescription = stringResource(R.string.lid),
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Fit
+                )
+            }
+        }
         Column(
             modifier = Modifier
-                .padding(bottom = 15.dp)
+                // .padding(bottom = 15.dp)
                 .width(IntrinsicSize.Max),
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
@@ -91,7 +131,6 @@ fun AboutUsScreen(repository: PlaylistRepository = koinInject(), navController: 
                     text = stringResource(R.string.aboutUs_donate)
                 )
             }
-
             OutlinedButton(
                 onClick = {
                     uriHandler.openUri("https://irfc.at/app/app-gewinnspiel/")
@@ -100,6 +139,57 @@ fun AboutUsScreen(repository: PlaylistRepository = koinInject(), navController: 
             ) {
                 Text(
                     text = stringResource(R.string.aboutUs_raffle),
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+            }
+            OutlinedButton(
+                onClick = {
+                    uriHandler.openUri("https://irfc.at/home/spendenkinderprojekte/")
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.ChildCare,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+                Text(
+                    modifier = Modifier.padding(start = 10.dp),
+                    text = stringResource(R.string.aboutUs_children),
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+            }
+            OutlinedButton(
+                onClick = {
+                    uriHandler.openUri("https://irfc.at/shop/")
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.ShoppingCart,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+                Text(
+                    modifier = Modifier.padding(start = 10.dp),
+                    text = stringResource(R.string.aboutUs_shop),
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+            }
+            OutlinedButton(
+                onClick = {
+                    uriHandler.openUri("https://irfc.at/am-event/#verlosung")
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.DirectionsCarFilled,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+                Text(
+                    modifier = Modifier.padding(start = 10.dp),
+                    text = stringResource(R.string.aboutUs_verlosung),
                     color = MaterialTheme.colorScheme.tertiary
                 )
             }
@@ -141,49 +231,53 @@ fun AboutUsScreen(repository: PlaylistRepository = koinInject(), navController: 
                 )
             }
         }
-
         Column(
             modifier = Modifier.padding(bottom = 15.dp)
                 .width(IntrinsicSize.Max),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            OutlinedButton(
-                onClick = { uriHandler.openUri("https://irfc.at/kontakt/impressum") },
-                modifier = Modifier.fillMaxWidth()
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    // .padding(vertical = 6.dp)
+                    .clickable { uriHandler.openUri("https://irfc.at/kontakt/impressum") },
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Icon(
                     imageVector = Icons.Outlined.Info,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.tertiary
+                    contentDescription = null
                 )
                 Text(
-                    modifier = Modifier.padding(start = 10.dp),
-                    text = stringResource(R.string.aboutUs_imprint),
-                    color = MaterialTheme.colorScheme.tertiary
+                    text = stringResource(R.string.aboutUs_imprint) + "  >",
+                    modifier = Modifier
+                        .padding(start = 10.dp)
+                        .weight(1f)
                 )
             }
-            OutlinedButton(
-                onClick = { uriHandler.openUri("https://irfc.at/kontakt/datenschutz") },
-                modifier = Modifier.fillMaxWidth()
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    // .padding(vertical = 6.dp) // controls height
+                    .clickable { uriHandler.openUri("https://irfc.at/kontakt/datenschutz") },
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Icon(
                     imageVector = Icons.Outlined.Shield,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.tertiary
+                    contentDescription = null
                 )
                 Text(
-                    modifier = Modifier.padding(start = 10.dp),
-                    text = stringResource(R.string.aboutUs_privacy),
-                    color = MaterialTheme.colorScheme.tertiary
+                    text = stringResource(R.string.aboutUs_privacy) + "  >",
+                    modifier = Modifier
+                        .padding(start = 10.dp)
+                        .weight(1f)
                 )
             }
         }
-
         Text(
             text = stringResource(R.string.aboutUs_providedBy),
             textAlign = TextAlign.Center,
             style = MaterialTheme.typography.bodySmall
         )
-        Spacer(modifier = Modifier.height(20.dp))
+        // Spacer(modifier = Modifier.height(20.dp))
     }
 }
