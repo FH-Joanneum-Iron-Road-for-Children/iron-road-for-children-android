bbf442352258683a141b3d71c23f0ec501c6a4da
Rudi <rudolf.berger@edu.fh-joanneum.at>
2025-05-13 22:20:02 +0200

    changed splash_logo_text Image to Text

--- app/src/main/java/at/irfc/app/ui/splashScreen/FullSplashScreen.kt
+++ app/src/main/java/at/irfc/app/ui/splashScreen/FullSplashScreen.kt
@@ -6,11 +6,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import at.irfc.app.R
 import at.irfc.app.ui.theme.IrfcBlue
 
@@ -35,10 +40,12 @@ fun FullSplashScreen() {
                 modifier = Modifier.height(40.dp)
             )
             Spacer(modifier = Modifier.height(8.dp))
-            Image(
-                painter = painterResource(R.drawable.splash_logo_text),
-                contentDescription = null,
-                modifier = Modifier.height(20.dp)
+            Text(
+                text = "ROCK.RIDE.GIVE! #IRFC2025",
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
             )
             Spacer(modifier = Modifier.height(32.dp))
             Image(
