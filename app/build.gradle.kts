642e855c7ec1a0369b4c4946422ea7e6d7fbf007
Rudi <rudolf.berger@edu.fh-joanneum.at>
2025-06-02 11:05:30 +0200

    test deployment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ android {
             buildConfigField(
                 type = "String",
                 name = "apiBaseUrl",
-                value = "\"https://backend.irfc.fh-joanneum.at/api/\""
+                value = "\"https://backend.irfc-test.fh-joanneum.at/api/\""
             )
             buildConfigField(
                 type = "String",
