3f5a755bcb0b0d2be2f7042e0ec2b9477e8a9238
Harald Schwab <harald.schwab2@fh-joanneum.at>
2026-07-22 09:00:08 +0200

    build: update ktlint version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ buildscript {
     }
 }
 plugins {
-    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
 
 subprojects {
